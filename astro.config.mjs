5211e3b1b66e422620a93ed4a8969110c82a3828
fatimahnaqib <fatimahnaqib987@gmail.com>
2025-08-07 14:26:19 -0700

    config changes

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,12 @@
-import { defineConfig } from 'astro/config'
+// @ts-check
+import { defineConfig } from "astro/config";
+import tailwindcss from "@tailwindcss/vite";
 
+// https://astro.build/config
 export default defineConfig({
   site: 'https://fatimahnaqib.github.io',
   base: '/devportfolio/',
-})
+  vite: {
+    plugins: [tailwindcss()],
+  },
+});
